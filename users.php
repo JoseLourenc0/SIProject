fbe65b2a5691519f7aa28eb083063949e0f43d74
joselourenco <joselourenco@flipfloplab.com.br>
2021-09-29 10:44:11 -0300

    Fixing nav menu for another pages and inserting users page

---
+++ users.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+
+$_SESSION['logged'] = $_SESSION['logged'] ?? false;
+
+if($_SESSION['logged']===false){
+    header('Location: index.php');
+}
+
+require_once 'layout/header.html';
+
+require_once 'layout/users.html';
+
+require_once 'layout/footer.html';
